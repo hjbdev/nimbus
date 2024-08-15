74d486804df8e373fdded37d73a29eb07da1cf07
hjbdev <hjbdev@users.noreply.github.com>
2024-08-15 15:29:54 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Hjbdev\Nimbus\Middleware\Authorize;
 use Hjbdev\Nimbus\Models\Exception;
-use Hjbdev\Nimbus\Models\TraceLine;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -42,7 +41,7 @@ Route::prefix(config('nimbus.route.prefix', 'nimbus'))->middleware([Authorize::c
             ->where('file', 'NOT LIKE', 'vendor/%')
             ->groupBy('file');
 
-        match($request->time) {
+        match ($request->time) {
             'week' => $exceptions = $exceptions->where('created_at', '>=', now()->subWeek()),
             'day' => $exceptions = $exceptions->where('created_at', '>=', now()->subDay()),
             'hour' => $exceptions = $exceptions->where('created_at', '>=', now()->subHour()),
@@ -60,7 +59,7 @@ Route::prefix(config('nimbus.route.prefix', 'nimbus'))->middleware([Authorize::c
         // get contents of the file
         if (file_exists($exception->file)) {
             $file = file_get_contents($exception->file);
-        } else if(file_exists(base_path($exception->file))) {
+        } elseif (file_exists(base_path($exception->file))) {
             $file = file_get_contents(base_path($exception->file));
         } else {
             $file = null;
@@ -75,7 +74,7 @@ Route::prefix(config('nimbus.route.prefix', 'nimbus'))->middleware([Authorize::c
             $start = max($exception->line - 10, 0);
             $end = min($exception->line + 10, count($file));
             $file = array_slice($file, $start, $end - $start, true);
-            
+
             // highlight the line of the exception
             // $file[$exception->line - 1] = '<span class="bg-red-500 text-white">' . $file[$exception->line - 1] . '</span>';
         }
--- src/Components/NavLink.php
+++ src/Components/NavLink.php
@@ -7,8 +7,8 @@ use Illuminate\View\Component;
 class NavLink extends Component
 {
     public function __construct(
-        public string $text = "",
-        public string $href = "",
+        public string $text = '',
+        public string $href = '',
         public bool $active = false
     ) {}
 
